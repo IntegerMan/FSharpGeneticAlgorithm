95dccaca3267c00270e8b687e4acda8909eb840a
Matt Eland <Matt.Eland@GMail.com>
2019-09-29 17:20:38 -0400

    Better structure

--- MattEland.FSharpGeneticAgorithm.ConsoleTestApp/Display.fs
+++ MattEland.FSharpGeneticAgorithm.ConsoleTestApp/Display.fs
@@ -1,8 +1,9 @@
-﻿namespace MattEland.FSharpGeneticAlgorithm.Logic
+﻿namespace MattEland.FSharpGeneticAlgorithm.ConsoleTestApp
 
+open System
 open MattEland.FSharpGeneticAlgorithm.Logic.World
 
-module Cells =
+module Display =
 
   let printCell char isLastCell =
     if isLastCell then
@@ -18,3 +19,10 @@ module Cells =
       // Determine which character should exist in this line
       let char = world.GetCharacterAtCell(x,y)
       printCell char (x = world.MaxX)
+
+  let getUserInput(): ConsoleKeyInfo =
+    printfn ""
+    printfn "Press R to regenerate or X to exit"
+  
+    Console.ReadKey(true)
+  
--- MattEland.FSharpGeneticAgorithm.ConsoleTestApp/Program.fs
+++ MattEland.FSharpGeneticAgorithm.ConsoleTestApp/Program.fs
@@ -1,33 +1,29 @@
 ﻿open System
 open MattEland.FSharpGeneticAlgorithm.Logic.World
-open MattEland.FSharpGeneticAlgorithm.Logic.Cells
-    
+open MattEland.FSharpGeneticAlgorithm.ConsoleTestApp.Display
+
+let generateWorld randomizer =
+  new World(8, 8, randomizer)
+  
 [<EntryPoint>]
 let main argv =
   printfn "F# Console Application Example by Matt Eland"
-  printfn ""
-
+  
   let randomizer = new Random()
 
   let mutable simulating: bool = true
-  let mutable world = new World(8, 8, randomizer)
+  let mutable world = generateWorld(randomizer)
 
   while simulating do
     displayWorld world
-  
-    printfn "Press R to regenerate or X to exit"
-    printfn ""
 
-    let key = Console.ReadKey(true)
+    let key = getUserInput()
+    
     Console.Clear()
 
     match key.Key with
     | ConsoleKey.X -> simulating <- false
-
-    | ConsoleKey.R -> 
-      printfn "Regenerating World"
-      world <- new World(8, 8, randomizer)
-      
+    | ConsoleKey.R -> world <- generateWorld(randomizer)
     | _ -> printfn "Invalid input '%c'" key.KeyChar
 
   0 // return an integer exit code
