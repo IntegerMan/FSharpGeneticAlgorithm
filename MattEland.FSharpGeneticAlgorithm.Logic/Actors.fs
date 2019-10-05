feec42fad2b0521f4a4b8c503712f16058d2bf1c
isaacabraham <isaac.abraham@gmail.com>
2019-10-05 21:14:40 +0200

    Sample refactorings

--- MattEland.FSharpGeneticAlgorithm.Logic/Actors.fs
+++ MattEland.FSharpGeneticAlgorithm.Logic/Actors.fs
@@ -1,41 +1,22 @@
-﻿namespace MattEland.FSharpGeneticAlgorithm.Logic
+﻿module MattEland.FSharpGeneticAlgorithm.Logic.Actors
 
 open MattEland.FSharpGeneticAlgorithm.Logic.WorldPos
 
-module Actors =
+type ActorKind =
+  | Squirrel of hasAcorn:bool
+  | Tree
+  | Acorn
+  | Rabbit
+  | Doggo
 
-  [<AbstractClass>]
-  type Actor(pos: WorldPos) =
-    member val Pos = pos with get, set
-    abstract member Character: char
+type Actor =
+  { Pos : WorldPos
+    ActorKind : ActorKind }
 
-  type Squirrel(pos: WorldPos, hasAcorn: bool) =
-    inherit Actor(pos)
-    member this.HasAcorn = hasAcorn
-    override this.Character = 'S'
-
-  let createSquirrel pos = new Squirrel(pos, false)
-  
-  type Tree(pos: WorldPos) =
-    inherit Actor(pos)
-    override this.Character = 't'
-
-  let createTree pos = new Tree(pos)
-
-  type Acorn(pos: WorldPos) =
-    inherit Actor(pos)
-    override this.Character = 'a'
-
-  let createAcorn pos = new Acorn(pos)
-
-  type Rabbit(pos: WorldPos) =
-    inherit Actor(pos)
-    override this.Character = 'R'
-
-  let createRabbit pos = new Rabbit(pos)
-
-  type Doggo(pos: WorldPos) =
-    inherit Actor(pos)
-    override this.Character = 'D'
-
-  let createDoggo pos = new Doggo(pos)
+let getChar actor =
+  match actor.ActorKind with
+  | Squirrel _ -> 'S'
+  | Tree _ -> 't'
+  | Acorn _ -> 'a'
+  | Rabbit _ -> 'R'
+  | Doggo _ -> 'D'
--- MattEland.FSharpGeneticAlgorithm.Logic/WorldPos.fs
+++ MattEland.FSharpGeneticAlgorithm.Logic/WorldPos.fs
@@ -1,15 +1,11 @@
-﻿namespace MattEland.FSharpGeneticAlgorithm.Logic
+﻿module MattEland.FSharpGeneticAlgorithm.Logic.WorldPos
 
-module WorldPos =
+type WorldPos = {X: int32; Y:int32}
 
-  type WorldPos = {X: int32; Y:int32}
+let newPos x y = {X = x; Y = y}
 
-  let newPos x y = {X = x; Y = y}
-
-  let isAdjacentTo (posA: WorldPos) (posB: WorldPos): bool =
-    let xDiff = abs (posA.X - posB.X)
-    let yDiff = abs (posA.Y - posB.Y)
-    let result = xDiff <= 1 && yDiff <= 1
-    result
-
-  let isSamePos p1 p2 = p1.X = p2.X && p1.Y = p2.Y
+let isAdjacentTo (posA: WorldPos) (posB: WorldPos): bool =
+  let xDiff = abs (posA.X - posB.X)
+  let yDiff = abs (posA.Y - posB.Y)
+  let result = xDiff <= 1 && yDiff <= 1
+  result
