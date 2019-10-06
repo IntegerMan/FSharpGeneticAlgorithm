af31fc6a2fc78af8ab2057719a4a96b483b04f18
Matt Eland <Matt.Eland@GMail.com>
2019-10-06 17:03:37 -0400

    Added XUnit Tests around positions

---
+++ MattEland.FSharpGeneticAlgorithm.Tests/Tests.fs
@@ -0,0 +1,19 @@
+module MattEland.FSharpGeneticAlgorithm.Tests
+
+open Xunit
+
+open MattEland.FSharpGeneticAlgorithm.Logic.WorldPos
+
+[<Theory>]
+[<InlineData(4, 2, 4, 1, true)>]
+[<InlineData(4, 2, 4, 0, false)>]
+let ``Point Adjaency Tests`` (x1, y1, x2, y2, expectedAdjacent) =
+    // Arrange
+    let pos1 = newPos x1 y1
+    let pos2 = newPos x2 y2
+
+    // Act
+    let isAdjacent = isAdjacentTo pos1 pos2
+
+    // Assert
+    Assert.Equal(expectedAdjacent, isAdjacent)
