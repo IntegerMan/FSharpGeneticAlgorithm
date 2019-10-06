ae99cbc25291f92910eff6cb1f5882f0f6a5c168
Matt Eland <Matt.Eland@GMail.com>
2019-10-05 22:40:08 -0400

    Fixed a few logic issues after the merge

--- MattEland.FSharpGeneticAgorithm.ConsoleTestApp/Program.fs
+++ MattEland.FSharpGeneticAgorithm.ConsoleTestApp/Program.fs
@@ -1,17 +1,8 @@
 ﻿open System
-open MattEland.FSharpGeneticAlgorithm.Logic.Actors
 open MattEland.FSharpGeneticAlgorithm.Logic.World
 open MattEland.FSharpGeneticAlgorithm.Logic.Simulator
 open MattEland.FSharpGeneticAlgorithm.ConsoleTestApp.Display
   
-type GameCommand =
-  | MoveLeft | MoveRight
-  | MoveUp | MoveDown
-  | MoveUpLeft | MoveUpRight
-  | MoveDownLeft | MoveDownRight
-  | Wait
-  | Restart
-
 type Command =
   | Action of GameCommand
   | Exit
@@ -24,14 +15,12 @@ let tryParseInput (info:ConsoleKeyInfo) =
     | ConsoleKey.DownArrow -> Some (Action MoveDown)
     | ConsoleKey.NumPad7 | ConsoleKey.Home  -> Some (Action MoveUpLeft)
     | ConsoleKey.NumPad9 | ConsoleKey.PageUp -> Some (Action MoveUpRight)
-    | ConsoleKey.NumPad1 | ConsoleKey.End -> Some (Action MoveDownRight)
+    | ConsoleKey.NumPad1 | ConsoleKey.End -> Some (Action MoveDownLeft)
     | ConsoleKey.NumPad3 | ConsoleKey.PageDown -> Some (Action MoveDownRight)
     | ConsoleKey.NumPad5 | ConsoleKey.Spacebar | ConsoleKey.Clear -> Some (Action Wait) 
     | ConsoleKey.X -> Some Exit
     | ConsoleKey.R -> Some (Action Restart)
     | _ -> None
-
-type GameState = { World : World; Player : Actor }
 
 [<EntryPoint>]
 let main argv =
@@ -41,32 +30,20 @@ let main argv =
     let r = Random()
     fun max -> (r.Next max) + 1
 
-  let endState =
-    let world = makeWorld 13 13 getRandomNumber
-    let player = world.Squirrel
-    let state = { World = world; Player = world.Squirrel }
+  let world = makeWorld 13 13 getRandomNumber
 
-    let playTurn state command =
+  let mutable state = { World = world; Player = world.Squirrel }
+  let mutable simulating: bool = true
+
+  while simulating do
+    let player = state.World.Squirrel
+    let userCommand = getUserInput(state.World) |> tryParseInput
+
+    match userCommand with
+    | Some command -> 
       match command with 
-      | MoveLeft -> { state with World = moveActor world player -1 0 }
-      | MoveRight -> { state with World = moveActor world player 1 0 } 
-      | MoveUp -> { state with World = moveActor world player 0 -1 } 
-      | MoveDown -> { state with World = moveActor world player 0 1 }
-      | MoveUpLeft  -> { state with World = moveActor world player -1 -1 }
-      | MoveUpRight -> { state with World = moveActor world player 1 -1 }
-      | MoveDownLeft -> { state with World = moveActor world player -1 1 } 
-      | MoveDownRight -> { state with World = moveActor world player 1 1 }
-      | Wait ->
-        printfn "Time Passes..."
-        state
-      | Restart ->
-        let world = makeWorld 13 13 getRandomNumber
-        { World = world; Player = world.Squirrel }
-
-    Seq.initInfinite(fun _ -> getUserInput())
-    |> Seq.choose tryParseInput
-    |> Seq.takeWhile (function | Exit -> false | _ -> true)
-    |> Seq.choose(function | Exit -> None | Action gameCommand -> Some gameCommand)
-    |> Seq.fold playTurn state
-
+      | Exit -> simulating <- false
+      | Action gameCommand -> state <- playTurn state player getRandomNumber gameCommand
+    | None -> printfn "Invalid input"
+    
   0 // return an integer exit code
--- MattEland.FSharpGeneticAlgorithm.Logic/Simulator.fs
+++ MattEland.FSharpGeneticAlgorithm.Logic/Simulator.fs
@@ -4,6 +4,8 @@ open MattEland.FSharpGeneticAlgorithm.Logic.WorldPos
 open MattEland.FSharpGeneticAlgorithm.Logic.World
 open MattEland.FSharpGeneticAlgorithm.Logic.Actors
 
+type GameState = { World : World; Player : Actor }
+
 let isValidPos pos (world: World): bool = 
   pos.X >= 1 && pos.Y >= 1 && pos.X <= world.MaxX && pos.Y <= world.MaxY
 
@@ -24,5 +26,31 @@ let moveActor world actor xDiff yDiff =
     | Doggo -> { world with Doggo = actor }
   else
     world
+
+type GameCommand =
+  | MoveLeft | MoveRight
+  | MoveUp | MoveDown
+  | MoveUpLeft | MoveUpRight
+  | MoveDownLeft | MoveDownRight
+  | Wait
+  | Restart
+
+let playTurn state player getRandomNumber command =
+  let world = state.World
+  match command with 
+  | MoveLeft -> { state with World = moveActor world player -1 0 }
+  | MoveRight -> { state with World = moveActor world player 1 0 } 
+  | MoveUp -> { state with World = moveActor world player 0 -1 } 
+  | MoveDown -> { state with World = moveActor world player 0 1 }
+  | MoveUpLeft  -> { state with World = moveActor world player -1 -1 }
+  | MoveUpRight -> { state with World = moveActor world player 1 -1 }
+  | MoveDownLeft -> { state with World = moveActor world player -1 1 } 
+  | MoveDownRight -> { state with World = moveActor world player 1 1 }
+  | Wait ->
+    printfn "Time Passes..."
+    state
+  | Restart ->
+    let world = makeWorld 13 13 getRandomNumber
+    { World = world; Player = world.Squirrel }
 
 // TODO: I'll need a way of simulating an actor's turn
