1ff0d1c2017799d94e6e401ed8195cf5eb18ff6e
Matt Eland <Matt.Eland@GMail.com>
2019-10-22 22:49:15 -0400

    WPF App now is a thing and displays a text grid.

---
+++ MattEland.FSharpGeneticAlgorithm.WindowsClient/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using MattEland.FSharpGeneticAlgorithm.Logic;
+
+namespace MattEland.FSharpGeneticAlgorithm.WindowsClient
+{
+    internal class MainViewModel
+    {
+        private readonly World.World _world;
+
+        public MainViewModel()
+        {
+            _world = WorldGeneration.makeTestWorld(false);
+        }
+
+        public string TextGrid => BuildAsciiGrid();
+
+        private string BuildAsciiGrid()
+        {
+            var sb = new StringBuilder();
+
+            for (int y = 1; y < _world.MaxY; y++)
+            {
+                for (int x = 1; x < _world.MaxX; x++)
+                {
+                    sb.Append(World.getCharacterAtCell(x, y, _world));
+                }
+
+                // Advance to the next line
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
